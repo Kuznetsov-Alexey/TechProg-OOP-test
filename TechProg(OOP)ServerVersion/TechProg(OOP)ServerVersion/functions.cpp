210949459cf66d519183e6017039b3628e93360c
Kuznetsov-Alexey <Kuznetsov-Alexey@github.com>
2019-02-26 15:23:16 +0700

    Начало, скопировал все файлы

---
+++ TechProg(OOP)ServerVersion/TechProg(OOP)ServerVersion/functions.cpp
@@ -0,0 +1,157 @@
+#include "header.h"
+
+void container::In(ifstream &ifst)
+{
+	while (!ifst.eof()) {
+		if ((container::current = type::In(ifst, current)) != 0) {
+			len++;
+		}
+	}
+}
+
+void container::Out(ofstream &ofst)
+{
+	ofst << "Container contents " << len
+		<< " elements." << endl;
+	for (int i = 0; i < len; i++) {
+		ofst << i + 1 << ": ";
+		if (len > 0)
+		{
+			current->Out(ofst, current);
+			current = current->next;
+		}
+	}
+}
+
+void container::Clear()
+{
+	while (len > 0)
+	{
+		if (len == 1)
+		{
+			type *temp = current;
+			current = NULL;
+			free(temp);
+			len = len - 1;
+			break;
+		}
+		type *temp;
+		temp = current->next;
+		while (temp->next != current)
+			temp = temp->next;
+
+		temp->next = current->next;
+		free(current);
+		current = temp;
+		len = len - 1;
+	}
+
+
+
+}
+
+container::container()
+{
+	current = NULL;
+	len = 0;
+}
+
+type* type::In(ifstream &ifst, type *current)
+{
+
+	type *temporary, *point;	//Временные указатели
+	int k;
+	//int size;
+	ifst >> k;
+	switch (k) {
+	case 1:
+		temporary = new diagonal; break;
+	case 2:
+		temporary = new matrix; break;
+	default:
+		return 0;
+	}
+
+	if (current == NULL)
+	{
+		temporary->mass = temporary->InData(ifst);
+		temporary->next = temporary; // указатель на сам корневой узел
+	}
+	else
+	{
+		temporary->mass = temporary->InData(ifst);
+		point = current->next; // сохранение указателя на следующий элемент
+		current->next = temporary; // предыдущий узел указывает на создаваемый
+		temporary->next = point; // созданный узел указывает на следующий элемент
+		current->mass = temporary->mass;
+	}
+	return temporary;
+
+
+}
+
+
+int * diagonal::InData(ifstream &ifst)
+{
+	ifst >> size;
+	mass = new int[size]; // массив значений эл-ов главной диагонали
+	for (int i = 0; i < size; i++)
+	{
+		ifst >> mass[i];
+	}
+	return(mass);
+}
+
+void diagonal::Out(ofstream &ofst, type *current)
+{
+	ofst << "It's simple matrix " << size << "x" << size << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		int j = size - i;
+		int k = i + 1;
+		while (j < size)
+		{
+			ofst << "0 ";
+			j++;
+		}
+
+		ofst << mass[i];
+
+		while (k < size)
+		{
+			ofst << " 0";
+			k++;
+		}
+
+		ofst << endl;
+	}
+	ofst << endl;
+}
+
+
+int * matrix::InData(ifstream &ifst)
+{
+	ifst >> size;
+	matrix::mass = new int[size*size]; // массив значений 
+
+	for (int i = 0; i < size*size; i++)
+	{
+		ifst >> mass[i];
+	}
+	return(mass);
+}
+
+void matrix::Out(ofstream &ofst, type *current)
+{
+	ofst << "It's casual matrix " << matrix::size << "x" << matrix::size << endl;
+	for (int i = 0; i < matrix::size; i++)
+	{
+		for (int j = 0; j < matrix::size; j++)
+		{
+			ofst << matrix::mass[i*matrix::size + j] << " ";
+		}
+		ofst << endl;
+	}
+	ofst << endl;
+}
---
+++ TechProg(OOP)ServerVersion/TechProg(OOP)ServerVersion/header.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "iostream"
+#include "fstream"
+
+using namespace std;
+
+class type
+{
+public:
+	// иденитфикация, порождение и ввод объекта из потока
+	static type* In(ifstream &ifst, type *current);				//Для ввода последующих эл-ов
+	virtual int * InData(ifstream &ifst) = 0;					// ввод с учетом типа объекта
+	virtual void Out(ofstream &ofst, type *current) = 0;		// вывод с учетом типа объекта
+
+	int *mass;
+	type *next;
+};
+
+
+class container
+{
+	int len;			//кол-во элементов
+
+public:
+
+	type *current;	//указатель на текущий элемент
+
+	void In(ifstream &ifst);	//ввод матриц
+	void Out(ofstream &ofst);	//вывод матриц
+	void Clear();				//очистка контейнера
+	container();				//инициализация контейнера
+};
+
+
+class diagonal : public type {
+	int size;
+	int *mass;
+public:
+	// переопределяем интерфейс класса
+	int * InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst, type *current); // вывод
+
+	diagonal() {} // создание без инициализации.
+};
+
+class matrix : public type {
+	int size;
+	int *mass;
+
+public:
+	// переопределяем интерфейс класса
+	int * InData(ifstream &ifst); // ввод
+	void Out(ofstream &ofst, type *current); // вывод
+	matrix() {} // создание без инициализации.
+};
